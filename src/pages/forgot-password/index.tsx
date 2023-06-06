5ddb8113df09b6013f3576df7b3769cf8baa6b03
Chris Carrillo <chris.carrillo@scoutandcellar.com>
2023-06-06 10:51:39 -0500

    feat: forgot password wip

--- src/pages/forgot-password/index.tsx
+++ src/pages/forgot-password/index.tsx
@@ -1,15 +1,62 @@
+import { useCallback, useMemo } from 'react'
+
 import { NextPage } from 'next'
 import { NextSeo } from 'next-seo'
+import { ReCAPTCHA } from 'react-google-recaptcha'
+import { SubmitHandler } from 'react-hook-form'
+import { z } from 'zod'
 
+import { Form } from '@/components/form'
+import { Input } from '@/core/components/input'
 import { Typography } from '@/core/components/typogrpahy'
 
-const ForgotPasswordPage: NextPage = () => (
-  <>
-    <NextSeo />
-    <main>
-      <Typography as="h1">Forgot Password</Typography>
-    </main>
-  </>
-)
+const forgotPasswordSchema = z.object({
+  email: z.string().min(1, { message: 'Please enter your email.' }),
+  recaptcha: z.string().min(1, { message: 'Please validate the captcha.' }),
+})
+
+type ForgotPasswordSchema = z.infer<typeof forgotPasswordSchema>
+
+const ForgotPasswordPage: NextPage = () => {
+  const defaultValues: ForgotPasswordSchema = useMemo(
+    () => ({
+      email: '',
+      recaptcha: '',
+    }),
+    []
+  )
+
+  const handleSubmit: SubmitHandler<ForgotPasswordSchema> = useCallback(async data => {
+    // ! TODO
+  }, [])
+
+  return (
+    <>
+      <NextSeo />
+
+      <main>
+        <div className="container mx-auto">
+          <div className="rounded mx-auto max-w-lg border border-neutral-light bg-neutral-50 p-4">
+            <Typography as="h1" displayAs="h4">
+              Forgot your password?
+            </Typography>
+            <Typography>Enter your email to be sent a link to reset your password.</Typography>
+            <Form
+              className=""
+              defaultValues={defaultValues}
+              schema={forgotPasswordSchema}
+              onSubmit={handleSubmit}
+            >
+              <Input label="Email" name="email" />
+              {process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY !== undefined ? (
+                <ReCAPTCHA sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY} />
+              ) : undefined}
+            </Form>
+          </div>
+        </div>
+      </main>
+    </>
+  )
+}
 
 export default ForgotPasswordPage
