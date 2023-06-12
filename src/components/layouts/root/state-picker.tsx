f79f85f12ce2190d09d0d3988052bf043a466921
Chris Carrillo <chris.carrillo@scoutandcellar.com>
2023-06-12 09:57:31 -0500

    feat: state warning

--- src/components/layouts/root/state-picker.tsx
+++ src/components/layouts/root/state-picker.tsx
@@ -1,15 +1,19 @@
 import { useCallback, useMemo } from 'react'
 
 import { Select, SelectProps, Skeleton } from '@mantine/core'
+import { modals } from '@mantine/modals'
+import { PrismicRichText } from '@prismicio/react'
 import { useQueryClient } from '@tanstack/react-query'
 
 import { PAGINATED_SEARCH_QUERY_KEY } from '@/features/search/queries'
+import { useStateWarningsQuery } from '@/features/state-warning/queries'
 import { useCartStorage } from '@/lib/hooks/use-cart-storage'
-import { CART_QUERY_KEY } from '@/lib/queries/cart'
+import { CART_QUERY_KEY, useCartQuery } from '@/lib/queries/cart'
 import { PAGINATED_PRODUCTS_QUERY_KEY, PRODUCTS_QUERY_KEY } from '@/lib/queries/products'
 import { useStatesQuery } from '@/lib/queries/state'
 import { useShippingStateStore } from '@/lib/stores/shipping-state'
-import { State } from '@/lib/types/index'
+import { Cart, State } from '@/lib/types/index'
+import { toastInfo } from '@/lib/utils/notifications'
 
 interface StatePickerProps {
   popup?: boolean
@@ -22,6 +26,8 @@ export const StatePicker = ({ popup = false }: StatePickerProps) => {
   const queryClient = useQueryClient()
   const [_, setCartStorage] = useCartStorage()
   const { data: states, isFetching, isLoading } = useStatesQuery()
+  const { data: stateWarnings } = useStateWarningsQuery()
+  const { data: cart } = useCartQuery()
   const { setShippingState, shippingState } = useShippingStateStore()
 
   const stateOptions = useMemo(
@@ -44,8 +50,52 @@ export const StatePicker = ({ popup = false }: StatePickerProps) => {
       queryClient.invalidateQueries([...PRODUCTS_QUERY_KEY])
       queryClient.invalidateQueries([...PAGINATED_PRODUCTS_QUERY_KEY])
       queryClient.invalidateQueries([PAGINATED_SEARCH_QUERY_KEY])
+
+      const removedCartItems: Cart['items'] = []
+
+      cart?.items.forEach(item => {
+        const availability = item.availability?.filter(
+          state => state.provinceId === newState?.provinceID && state.enabled
+        )
+
+        if (!availability || !availability.length) {
+          removedCartItems.push(item)
+        }
+      })
+
+      const removedString = removedCartItems.map(product => product.displayName).join(', ')
+      if (removedString !== '') {
+        toastInfo({
+          message: `${removedString.replace(
+            /<[^>]*>?/gm,
+            ''
+          )} has been removed from your cart because it is not available in the state you are
+    shipping to.`,
+        })
+      }
+
+      if (stateWarnings !== undefined) {
+        const data = stateWarnings.data.states.find(
+          state => state.state?.toLowerCase() === newState?.abbreviation.toLowerCase()
+        )
+
+        if (data !== undefined) {
+          modals.open({
+            centered: true,
+            children: (
+              <div className="[&>*]:mb-2 [&>h3]:!h5">
+                <PrismicRichText field={data?.warning} />
+              </div>
+            ),
+            classNames: {
+              title: '!text-2xl',
+            },
+            title: data?.modal_name,
+          })
+        }
+      }
     },
-    [queryClient, setCartStorage, setShippingState, states]
+    [cart?.items, queryClient, setCartStorage, setShippingState, stateWarnings, states]
   )
 
   const filter: SelectProps['filter'] = useCallback(
---
+++ src/features/state-warning/queries/index.ts
@@ -0,0 +1,25 @@
+import { Client, Content } from '@prismicio/client'
+import { useQuery } from '@tanstack/react-query'
+
+import { createClient } from '@/prismic-io'
+
+export const getStateWarnings = async (client?: Client<Content.AllDocumentTypes>) => {
+  try {
+    const prismicClient = client || createClient()
+    const warnings = await prismicClient.getSingle('state_warning')
+
+    return warnings
+  } catch {
+    return undefined
+  }
+}
+
+export const STATE_WARNINGS_QUERY_KEY = 'state-warnings'
+
+export const useStateWarningsQuery = () =>
+  useQuery({
+    cacheTime: 15 * (60 * 1000), // 15 mins
+    queryFn: () => getStateWarnings(),
+    queryKey: [STATE_WARNINGS_QUERY_KEY],
+    staleTime: 10 * (60 * 1000), // 10 mins
+  })
