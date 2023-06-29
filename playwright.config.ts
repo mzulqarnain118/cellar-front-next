42eab6176b1a60ab640b540a221bd8eb1b54b30d
Chris Carrillo <chris.carrillo@scoutandcellar.com>
2023-06-29 14:15:47 -0500

    ci: remove chrome from playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -45,10 +45,10 @@ export default defineConfig({
     //   name: 'Microsoft Edge',
     //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
     // },
-    {
-      name: 'Google Chrome',
-      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
   ],
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html'], ['junit', { outputFile: 'results.xml' }]],
