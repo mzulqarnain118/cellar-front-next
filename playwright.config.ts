360d3ce2a9d41bd05e5993b68f0a00834b29aec5
Chris Carrillo <chris.carrillo@scoutandcellar.com>
2023-07-05 11:21:51 -0500

    ci: update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -57,8 +57,6 @@ export default defineConfig({
   testDir: './e2e',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://127.0.0.1:3000',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
@@ -66,7 +64,7 @@ export default defineConfig({
   webServer: {
     command: 'pnpm dev',
     reuseExistingServer: !process.env.CI,
-    url: 'http://127.0.0.1:3000',
+    url: 'http://localhost:3000',
   },
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
