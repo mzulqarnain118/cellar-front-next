cc178551e5b81f0e587d476b91fad2912f2a9b68
Ayyaz Ali <ayyazalifurqan@gmail.com>
2023-08-28 19:24:24 +0500

    fix: code structure

--- src/components/product-card/product-image-link.tsx
+++ src/components/product-card/product-image-link.tsx
@@ -1,24 +1,27 @@
-import { memo } from 'react'
+import { MouseEventHandler, memo } from 'react'
 
-import { CartItem, SubscriptionProduct } from '@/lib/types'
-import { trackProductSelected } from '@/lib/utils/gtm-util'
 import { BlurImage } from '../blur-image'
 import { Link } from '../link'
 
 interface ProductImageLinkProps {
-  product: SubscriptionProduct | CartItem
   cartUrl: string
   displayName: string
   pictureUrl?: string
   priority?: boolean
+  onProductClick: MouseEventHandler<HTMLAnchorElement>
 }
 
-
 export const ProductImageLink = memo(
-  ({ product, cartUrl, displayName, pictureUrl, priority = false }: ProductImageLinkProps) => (
+  ({
+    cartUrl,
+    displayName,
+    pictureUrl,
+    priority = false,
+    onProductClick,
+  }: ProductImageLinkProps) => (
     <figure className="flex items-center self-center justify-self-center">
       {pictureUrl !== undefined ? (
-        <Link className="relative w-60 h-60" onClick={() => trackProductSelected(product)} href={`/product/${cartUrl}`}>
+        <Link className="relative w-60 h-60" href={`/product/${cartUrl}`} onClick={onProductClick}>
           <BlurImage
             fill
             alt={displayName || 'Product'}
