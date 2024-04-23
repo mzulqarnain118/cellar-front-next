99ade4ab4a4353f186a3b2b0c4b0b995603ea9a5
Haider <haider.saleem@scoutandcellar.com>
2024-04-23 11:46:44 -0400

    fix: changes in ticket #26

--- src/components/layouts/root/header/index.tsx
+++ src/components/layouts/root/header/index.tsx
@@ -377,7 +377,7 @@ export const Header = () => {
         </div>
       </div>
       <div className="bg-[#f5f3f2]">
-        <div className="2xl:container mx-auto flex items-center  lg:pr-3 justify-between py-3 lg:w-full">
+        <div className="2xl:container mx-auto flex items-center xl:overflow-hidden  lg:pr-3 justify-between py-3 lg:w-full">
           {isDesktop ? (
             <>
               <div className="flex w-full items-center">
