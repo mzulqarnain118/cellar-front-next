3c4f5b58b7477ea1edcdde0e1fcd05d46b555836
Haider Saleem <Haider.Saleem@scoutandcellar.com>
2024-08-30 11:03:18 -0400

    fix: ticket #138

--- src/features/contact/components/form/index.tsx
+++ src/features/contact/components/form/index.tsx
@@ -258,17 +258,29 @@ export const ContactForm = () => {
         Contact us
       </Typography>
       <form
-        className="grid lg:grid-cols-2 lg:gap-x-10 items-start"
+        className="grid md:grid-cols-2 md:gap-x-10 gap-x-6 items-start "
         onSubmit={handleSubmit(onSubmit)}
       >
-        <Input error={errors.fullName?.message} label="Full name" {...register('fullName')} />
-        <Input error={errors.email?.message} label="Email" type="email" {...register('email')} />
+        <Input
+          error={errors.fullName?.message}
+          label="Full name"
+          {...register('fullName')}
+          className="md:col-span-1 col-span-2"
+        />
+        <Input
+          error={errors.email?.message}
+          label="Email"
+          type="email"
+          {...register('email')}
+          className="md:col-span-1 col-span-2"
+        />
         <Input
           error={errors.phoneNumber?.message}
           label="Phone number"
           type="tel"
           maxLength={10}
           {...register('phoneNumber')}
+          className="md:col-span-1 col-span-2"
         />
         <Input
           error={errors.orderNumber?.message}
@@ -276,15 +288,17 @@ export const ContactForm = () => {
           label="Order number"
           type="tel"
           {...register('orderNumber')}
+          className="md:col-span-1 col-span-2"
         />
         <Input
-          className="lg:col-span-2"
+          className="w-[100%] md:col-span-1 col-span-2"
           error={errors.consultantName?.message}
           instructionLabel="optional"
           label="Consultant name"
           {...register('consultantName')}
         />
         <Select
+          className="md:mt-[1rem]"
           data={[
             { value: 'Account Inquiry', label: 'Account Inquiry' },
             { value: 'General Inquiry', label: 'General Inquiry' },
@@ -314,10 +328,11 @@ export const ContactForm = () => {
           }}
         /> */}
         <Textarea
-          className="lg:col-span-2"
+          className="md:col-span-1 col-span-2"
           error={errors.body?.message}
           label="How can we help you?"
           {...register('body')}
+          noSpacing
         />
         {/* <FileInput
           clearable
