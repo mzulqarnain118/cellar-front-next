1b142280b8755ccae6efbdf322ccb8ebe76a083a
Haider <haider.saleem@scoutandcellar.com>
2024-10-17 14:34:36 -0400

    fix: ticket # 153 fix ts issue

--- src/components/CodeSnippet/index.tsx
+++ src/components/CodeSnippet/index.tsx
@@ -1,38 +1,57 @@
-import React from "react";
+import React, { useEffect } from 'react'
 
+interface CodeSnippetComponentProps {
+  slice: {
+    primary: {
+      rich_text: Array<{
+        text: string
+      }>
+    }
+  }
+}
 
-
-export default function CodeSnippetComponent({ slice }) {
-
-  React.useEffect(() => {
-    const tastryScript = slice?.primary?.rich_text?.find(el => el?.text?.includes("</script"))?.text;
-    if(!tastryScript) return
+const CodeSnippetComponent: React.FC<CodeSnippetComponentProps> = ({ slice }) => {
+  useEffect(() => {
+    const tastryScript = slice?.primary?.rich_text?.find(el => el?.text?.includes('</script'))?.text
+    if (!tastryScript) return
 
     // Create the script element
-    const script = document.createElement("script");
+    const script = document.createElement('script')
 
-    const srcStart = tastryScript?.indexOf('src="') + 5; // Add 5 to skip past 'src="'
-    const srcEnd = tastryScript?.indexOf('"', srcStart);
-
+    const srcStart = tastryScript?.indexOf('src="') + 5 // Add 5 to skip past 'src="'
+    const srcEnd = tastryScript?.indexOf('"', srcStart)
 
     script.src = tastryScript.substring(srcStart, srcEnd)
 
     // Attach onload handler properly
     script.onload = () => {
-      const tastryEvent = new Event('load');
-      window.dispatchEvent(tastryEvent);
-    };
+      const tastryEvent = new Event('load')
+      window.dispatchEvent(tastryEvent)
+      script.blur()
+    }
 
     // Append the script to the body
-    document.body.appendChild(script);
+    document.body.appendChild(script)
 
     // Clean up and remove the script when the component is unmounted or refreshed
     return () => {
       if (script && document.body.contains(script)) {
-        document.body.removeChild(script);
+        document.body.removeChild(script)
       }
-    };
-  }, [slice?.primary?.rich_text]);
+    }
+  }, [slice?.primary?.rich_text])
 
-  return <div>{ slice?.primary?.rich_text?.find(el => el?.text?.includes("</div")) &&  <div  dangerouslySetInnerHTML={{ __html: slice?.primary?.rich_text?.find(el => el?.text?.includes("</div"))?.text} } />}</div>
+  return (
+    <div>
+      {slice?.primary?.rich_text?.find(el => el?.text?.includes('</div')) && (
+        <div
+          dangerouslySetInnerHTML={{
+            __html: slice?.primary?.rich_text?.find(el => el?.text?.includes('</div'))?.text || '',
+          }}
+        />
+      )}
+    </div>
+  )
 }
+
+export default CodeSnippetComponent
