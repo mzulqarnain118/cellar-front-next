6e5d23143ee70944faffeb6bf259d3830c19f5a0
Haider Saleem <haider.saleem@scoutandcellar.com>
2025-03-24 10:51:29 -0400

    fix: ticket #193

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,8 +16,8 @@ import { PrismicProvider } from '@prismicio/react'
 import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister'
 import { Hydrate, QueryClient } from '@tanstack/react-query'
 import {
-  PersistQueryClientOptions,
-  PersistQueryClientProvider,
+    PersistQueryClientOptions,
+    PersistQueryClientProvider,
 } from '@tanstack/react-query-persist-client'
 import { SessionProvider } from 'next-auth/react'
 import { DefaultSeo } from 'next-seo'
@@ -39,93 +39,93 @@ import { theme } from 'theme'
 import '../globals.css'
 
 const ProgressBar = dynamic(
-  () => import('src/components/progress-bar').then(module => module.ProgressBar),
-  { ssr: false }
+    () => import('src/components/progress-bar').then(module => module.ProgressBar),
+    { ssr: false }
 )
 const merriweather = Merriweather({
-  subsets: ['latin'],
-  variable: '--font-merriweather',
-  weight: '400',
+    subsets: ['latin'],
+    variable: '--font-merriweather',
+    weight: '400',
 })
 
 const persister = createSyncStoragePersister({
-  storage: typeof window !== 'undefined' ? window.localStorage : undefined,
+    storage: typeof window !== 'undefined' ? window.localStorage : undefined,
 })
 
 const persistOptions: Omit<PersistQueryClientOptions, 'queryClient'> = {
-  dehydrateOptions: {
-    shouldDehydrateQuery: query => Boolean(query.meta?.persist) ?? false,
-  },
-  persister,
+    dehydrateOptions: {
+        shouldDehydrateQuery: query => Boolean(query.meta?.persist) ?? false,
+    },
+    persister,
 }
 
 const ReactQueryDevtoolsProduction = lazy(() =>
-  import('@tanstack/react-query-devtools/build/lib/index.prod.js').then(d => ({
-    default: d.ReactQueryDevtools,
-  }))
+    import('@tanstack/react-query-devtools/build/lib/index.prod.js').then(d => ({
+        default: d.ReactQueryDevtools,
+    }))
 )
 
 export const reportWebVitals = ({ id, label, name, value }: NextWebVitalsMetric) => {
-  event(name, {
-    category: label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
-    label: id,
-    nonInteraction: true,
-    value: Math.round(name === 'CLS' ? value * 1000 : value),
-  })
+    event(name, {
+        category: label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
+        label: id,
+        nonInteraction: true,
+        value: Math.round(name === 'CLS' ? value * 1000 : value),
+    })
 }
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
-  const isDesktop = useIsDesktop()
-  const router = useRouter()
+    const isDesktop = useIsDesktop()
+    const router = useRouter()
 
-  const { cartOpen } = useCartOpen()
-  const [showDevtools, setShowDevtools] = useState(false)
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: { refetchOnWindowFocus: false },
-        },
-      })
-  )
+    const { cartOpen } = useCartOpen()
+    const [showDevtools, setShowDevtools] = useState(false)
+    const [queryClient] = useState(
+        () =>
+            new QueryClient({
+                defaultOptions: {
+                    queries: { refetchOnWindowFocus: false },
+                },
+            })
+    )
 
-  useEffect(() => {
-    const tagManagerArgs: TagManagerArgs = {
-      gtmId: process.env.NEXT_PUBLIC_GTM_ID ?? '',
-    }
+    useEffect(() => {
+        const tagManagerArgs: TagManagerArgs = {
+            gtmId: process.env.NEXT_PUBLIC_GTM_ID ?? '',
+        }
 
-    if (process.env.NEXT_PUBLIC_IS_PRODUCTION === 'false') {
-      window.toggleDevtools = () => setShowDevtools(prev => !prev)
-      setShowDevtools(true)
-    }
+        if (process.env.NEXT_PUBLIC_IS_PRODUCTION === 'false') {
+            window.toggleDevtools = () => setShowDevtools(prev => !prev)
+            setShowDevtools(true)
+        }
 
-    if (!isInitialized()) {
-      init({ orgId: process.env.NEXT_PUBLIC_FULLSTORY_ORG_ID || '' })
-    }
-    TagManager.initialize(tagManagerArgs)
-  }, [])
+        if (!isInitialized()) {
+            init({ orgId: process.env.NEXT_PUBLIC_FULLSTORY_ORG_ID || '' })
+        }
+        TagManager.initialize(tagManagerArgs)
+    }, [])
 
-  return (
-    <SessionProvider session={session}>
-      <PersistQueryClientProvider client={queryClient} persistOptions={persistOptions}>
-        <Hydrate state={pageProps.dehydratedState}>
-          <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
-            <Theme dataTheme="garden">
-              <ModalsProvider modals={modals}>
-                <Notifications position={isDesktop ? 'top-right' : 'top-right'} />
-                <PrismicProvider
-                  internalLinkComponent={InternalLink}
-                  linkResolver={linkResolver}
-                  richTextComponents={richTextComponents}
-                >
-                  <PrismicPreview repositoryName={repositoryName}>
-                    <DefaultSeo {...defaultSEOConfig} />
-                    <RootLayout>
-                      <Head>
-                        <meta content="width=device-width, initial-scale=1" name="viewport" />
-                      </Head>
-                      <style global jsx>
-                        {`
+    return (
+        <SessionProvider session={session}>
+            <PersistQueryClientProvider client={queryClient} persistOptions={persistOptions}>
+                <Hydrate state={pageProps.dehydratedState}>
+                    <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
+                        <Theme dataTheme="garden">
+                            <ModalsProvider modals={modals}>
+                                <Notifications position={isDesktop ? 'top-right' : 'top-right'} />
+                                <PrismicProvider
+                                    internalLinkComponent={InternalLink}
+                                    linkResolver={linkResolver}
+                                    richTextComponents={richTextComponents}
+                                >
+                                    <PrismicPreview repositoryName={repositoryName}>
+                                        <DefaultSeo {...defaultSEOConfig} />
+                                        <RootLayout>
+                                            <Head>
+                                                <meta content="width=device-width, initial-scale=1" name="viewport" />
+                                            </Head>
+                                            <style global jsx>
+                                                {`
                           :root {
                             --font-merriweather: ${merriweather.style.fontFamily};
                           }
@@ -141,21 +141,27 @@ const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
 
                           .linc-web-chat #linc-web-chat-iframe {
                             display: ${router.asPath.includes('product/') && !isDesktop
-                              ? 'none'
-                              : 'initial'};
+                                                        ? 'none'
+                                                        : 'initial'};
                             right: ${cartOpen ? '453px !important' : '13px'};
                           }
                         `}
-                      </style>
-                      <GoogleAnalytics trackPageViews />
-                      <Script
-                        async
-                        defer
-                        id="hs-script-loader"
-                        src="//js.hs-scripts.com/4801108.js"
-                        type="text/javascript"
-                      />
-                      <Script id="handleTastryAddToCart">{`
+                                            </style>
+                                            <GoogleAnalytics trackPageViews />
+                                            <Script
+                                                async
+                                                defer
+                                                id="hs-script-loader"
+                                                src="//js.hs-scripts.com/4801108.js"
+                                                type="text/javascript"
+                                            />
+                                            <Script
+                                                async
+                                                defer
+                                                src="https://cdn.userway.org/widget.js" data-account="CsK55iAciy"
+                                                type="text/javascript"
+                                            />
+                                            <Script id="handleTastryAddToCart">{`
                         window.handleTastryAddToCart = (sku) => {
                           if (typeof sku === 'string') {
                             window.sessionStorage.setItem('tastry-sku', JSON.stringify(sku.toLowerCase()));
@@ -164,29 +170,29 @@ const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
                           }
                         };
                     `}</Script>
-                      <Script
-                        id="tastry-quiz"
-                        src="https://tastry-react-login.azurewebsites.net/js/tastry_scout.js"
-                      />
+                                            <Script
+                                                id="tastry-quiz"
+                                                src="https://tastry-react-login.azurewebsites.net/js/tastry_scout.js"
+                                            />
 
-                      <Component {...pageProps} />
+                                            <Component {...pageProps} />
 
-                      <ProgressBar />
-                    </RootLayout>
-                  </PrismicPreview>
-                </PrismicProvider>
-              </ModalsProvider>
-            </Theme>
-          </MantineProvider>
-          {showDevtools ? (
-            <Suspense fallback={<></>}>
-              <ReactQueryDevtoolsProduction />
-            </Suspense>
-          ) : undefined}
-        </Hydrate>
-      </PersistQueryClientProvider>
-    </SessionProvider>
-  )
+                                            <ProgressBar />
+                                        </RootLayout>
+                                    </PrismicPreview>
+                                </PrismicProvider>
+                            </ModalsProvider>
+                        </Theme>
+                    </MantineProvider>
+                    {showDevtools ? (
+                        <Suspense fallback={<></>}>
+                            <ReactQueryDevtoolsProduction />
+                        </Suspense>
+                    ) : undefined}
+                </Hydrate>
+            </PersistQueryClientProvider>
+        </SessionProvider>
+    )
 }
 
 export default App
