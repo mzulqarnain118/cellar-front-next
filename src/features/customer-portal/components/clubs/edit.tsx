b536feebac8eefebb084c251d3bd6871e34f112c
Haider Saleem <haider.saleem@scoutandcellar.com>
2025-04-07 14:48:47 -0400

    fix: ticket #197 redirection issue

--- src/features/customer-portal/components/clubs/edit.tsx
+++ src/features/customer-portal/components/clubs/edit.tsx
@@ -362,7 +362,7 @@ export const ClubsEdit = ({
   )
 
   const handleCancel = useCallback(() => {
-    router.push(`${MY_ACCOUNT_PAGE_PATH}/clubs/${subscriptionId}`)
+    router.push(`${MY_ACCOUNT_PAGE_PATH}/${router?.asPath?.includes("auto-sips") ? 'auto-sips' : 'clubs'}/${subscriptionId}`)
   }, [router, subscriptionId])
 
   const handleSave = useCallback(() => {
